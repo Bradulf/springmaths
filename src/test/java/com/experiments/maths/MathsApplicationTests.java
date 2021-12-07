2ada9ac4e597788b19c9dd6e8e01c997aaaca51c
Brad <bradkharris@gmail.com>
2021-12-07 13:53:19 -0700

    first

---
+++ src/test/java/com/experiments/maths/MathsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiments.maths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
